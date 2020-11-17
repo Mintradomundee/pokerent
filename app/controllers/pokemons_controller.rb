1839848a06603098a0a7a876376bdcbb44944464
olaf herzig <olaf.s.herzig@gmail.com>
2020-11-17 17:48:43 +0100

    implementing pundit

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,19 +1,23 @@
 class PokemonsController < ApplicationController
+  
   def index
-    @pokemons = Pokemon.all
+    @pokemons = policy_scope(Pokemon).order(created_at: :desc)
   end
   
   def show
     @pokemon = Pokemon.find(params[:id])
+    authorize @pokemon
   end
 
   def new
     @pokemon = Pokemon.new
+    authorize @pokemon
   end
 
   def create
     @pokemon = Pokemon.new(pokemon_params) 
     @pokemon.user = current_user
+    authorize @pokemon
    if @pokemon.save
       redirect_to pokemon_path(@pokemon)
     else
@@ -23,10 +27,12 @@ class PokemonsController < ApplicationController
 
   def edit
     @pokemon = Pokemon.find(params[:id])
+    authorize @pokemon
   end
 
   def update
     @pokemon = Pokemon.find(params[:id])
+    authorize @pokemon
     if @pokemon.update(pokemon_params) 
       redirect_to pokemon_path(@pokemon)
     else
@@ -37,6 +43,7 @@ class PokemonsController < ApplicationController
 
   def destroy
     @pokemon = Pokemon.find(params[:id])
+    authorize @pokemon
     @pokemon.destroy
 
     redirect_to pokemons_path
