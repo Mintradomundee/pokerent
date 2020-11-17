a002b84665e3b15341908e606da2052afdc1d25d
Mintra Domundee <mintra.mynt@gmail.com>
2020-11-17 18:25:09 +0700

    MVC for create a new pokemon

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,3 +1,8 @@
 class Pokemon < ApplicationRecord
+  TYPE_POKEMON = ["Grass", "Fairy", "Fire", "Dragon", "Bug", "Ice", "Fighting", "Poison", "Flying", "Rock", "Normal", "water", "Ghost"]
+
   belongs_to :user
+  validates :name, :description, presence: true
+  validates :level, presence: true, numericality: { only_integer: true }, inclusion: { in: (1..3) }
+  validates :type_pokemon, presence: true, inclusion: { in: TYPE_POKEMON }
 end
