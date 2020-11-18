dc869219622acbf663a8fce805ece0f187d24dbb
olaf herzig <olaf.s.herzig@gmail.com>
2020-11-18 16:25:37 +0100

    cleaning up master

--- db/migrate/20201118133337_rename_pokemon_type_to_type_pokemon.rb
+++
@@ -1,5 +0,0 @@
-class RenamePokemonTypeToTypePokemon < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :pokemons, :type, :type_pokemon
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_18_133337) do
+ActiveRecord::Schema.define(version: 2020_11_18_104602) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
